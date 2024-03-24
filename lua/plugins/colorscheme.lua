8f24918886188cc7cd6469987178d6620f93207e
ttuijn <tim.tuijn@gmail.com>
2024-03-24 16:24:00 +0100

    Initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "Th3Whit3Wolf/one-nvim",
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+  },
+  {
+    "projekt0n/github-nvim-theme",
+  },
+  {
+    "neanias/everforest-nvim",
+  },
+  {
+    "rose-pine/neovim",
+  },
+  {
+    "rmehri01/onenord.nvim",
+  },
+  {
+    "navarasu/onedark.nvim",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
