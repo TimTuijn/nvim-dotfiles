a7856c8fee21525b924ec6d7c9d06f57b7775b11
ttuijn <tim.tuijn@gmail.com>
2024-04-01 22:10:44 +0200

    Extra plugins ; almost done with initial setup (non-LSP)

---
+++ TODO_quick.md
@@ -0,0 +1,5 @@
+# QUICK TODO
+
+- test GITUI
+- move further with last extras: https://www.lazyvim.org/extras/util/gitui
+
--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -25,6 +25,14 @@ return {
     },
 
     {
+      "<leader>fm",
+      function()
+        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+      end,
+      desc = "Open mini.files (Directory of Current File)",
+    },
+
+    {
       "<leader>E",
       function()
         require("mini.files").open(vim.uv.cwd(), true)
